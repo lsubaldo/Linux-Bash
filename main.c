7c3af15edb13e2d8543513cc22291c41e194e992
lsubaldo <lsubaldo@colgate.edu>
2015-10-05 23:25:45 -0400

    Added code for sequential and parallel modes

--- main.c
+++ main.c
@@ -63,9 +63,11 @@ void free_tokens(char **tokens) {
 int main(int argc, char **argv) {
 
 	// mode settings
-	int sequential = 0;
-	int parallel = 1; 
+	//sequential = 0;
+	//parallel = 1; 
 	int mode = 0;
+	int change = 0;
+	char mode_arr[] = {"sequential", "parallel"};
 
 	// command prompt loop
 	char * prompt = "Prompt: ";
@@ -86,18 +88,94 @@ int main(int argc, char **argv) {
 		}
 
 		char*** command_list = malloc((chunk_count+1)*(sizeof(char**));
-    	char* whitespace = " \t\n";
+    		char* whitespace = " \t\n";
 		for (int i = 0; i < chunk_count; i++) {
 			command_list[i] = parse(command_chunks[i], 
 			whitespace);
 		}
 		command_list[chunk_count] = NULL;
 
-		/* check that commands and option arguments are valid 
-			- includes looking at list(?) of commands
-			- checking that the command options are valid 
-			(another little lib? more arrays of pointers? 
-			- if mode change, set boolean */
+		if (mode == 0) {       // sequential mode
+			int i = 0;
+	  	    while (command_list[i] != NULL) {
+				int j = 0;
+  				if (strcmp(command_list[i][j], "exit")) {
+			    	printf("Exit called. Goodbye.");
+			    	fflush(stdout);
+			    	exit(); 
+				}
+				else if (strcmp(command_list[i][j], "mode")) {
+		   	    	j++;
+			    	if (*command_list[i][j] == NULL) {
+						printf("Current mode: %s\n", mode_arr[mode]);
+			    	}
+			    	else if (*command_list[i][j] == 's') {
+						change = 0;
+			    	}
+			    	else if (*command_list[i][j] == 'p') {
+						change = 1;
+			    	}
+				}
+				else {
+			    	pid_t p = fork(); 
+			    	if (p == 0) {
+						if (execv(command_list[i][j], command_list[i]) < 0) {
+			           		fprintf(stderr, "execv failed: %s\n", strerror(errno));
+						}
+			    	}
+			    	else if (p > 0) {
+						wait(&p);
+			    	}
+				}
+				i++;
+			}
+
+		else if (mode == 1) { //parallel
+			int i = 0;
+			while (command_list[i] != NULL) {
+				int j = 0;
+  				if (strcmp(command_list[i][j], "exit")) {
+			    	printf("Exit called. Goodbye.");
+			    	fflush(stdout);
+			    	exit(); 
+				}
+				else if (strcmp(command_list[i][j], "mode")) {
+		   	    	j++;
+			    	if (*command_list[i][j] == NULL) {
+						printf("Current mode: %s\n", mode_arr[mode]);
+			    	}
+			    	else if (*command_list[i][j] == 's') {
+						change = 0;
+			    	}
+			    	else if (*command_list[i][j] == 'p') {
+						change = 1;
+			    	}
+				}
+				else {
+					pid_t pid_array[chunk_count+1];// last idx == NULL?
+					int arr_idx = 0;
+					int k = 0;	
+					int worked = 0; //Kt
+					while (k < chunk_count) {
+						pid_t p = fork(); 
+						if (p == 0) {
+							if (execv(command_list[i][j], command_list[i]) < 0) {
+			           			fprintf(stderr, "execv failed: %s\n", strerror(errno));	
+								/*worked = 0; //Kt's crap 
+							}
+							else {
+								worked = 1; */
+							}	
+						}
+						else if (p > 0) {
+						}
+						k++;
+					}
+				}
+		}	
+			
+		 
+		/*- if mode change, set boolean */
 
 		/* check for exit command. If present, exit. */
 
